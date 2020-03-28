21ef84129c4f40b24f6e5710104bec2a4e3e915a
YANYU ZHANG <55149684+zhangyanyu0722@users.noreply.github.com>
2020-03-28 17:30:11 -0400

    Add files via upload

---
+++ codes/translate_voice.py
@@ -0,0 +1,34 @@
+# Copyright (c) 2020 Yanyu Zhang zhangya@bu.edu All rights reserved.
+import requests
+import speech_recognition as sr
+import pyaudio
+import wave
+
+def translate_voice(source_language, target_language):
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        audio_data = r.record(source, duration=5)
+        print("Translating...")
+        content = r.recognize_google(audio_data, language=source_language)
+
+    url = "https://translation.googleapis.com/language/translate/v2"
+    data = {
+        'key':"AIzaSyAAyweygngyKRrUkglrTTq8oDJ4PCWsdYo",
+        'source': source_language,
+        'target': target_language,
+        'q': content,
+        'format': "text"
+    }
+
+    response = requests.post(url, data)
+    res = response.json()
+    result = res["data"]["translations"][0]["translatedText"]
+    print(str(content)+"  --->  "+str(result))
+    return result
+
+if __name__ == '__main__':
+	# https://cloud.google.com/translate/docs/languages   -->  查询语言的简称
+    source = 'zh'
+    target = 'en'
+    result = translate_voice(source, target)
+
